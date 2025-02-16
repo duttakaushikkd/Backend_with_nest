b70996fbb430c9d449b8417a6fdc3f106b68ca8a
duttakaushikkd <duttakaushikkd@gmail.com>
2025-02-16 18:12:02 +0530

    Added API and code documentation

--- src/posts/post.controller.ts
+++ src/posts/post.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from "@nestjs/common";
 import { PostService } from "./providers/post.service";
 import { UsersService } from "src/users/providers/users.service";
+import { ApiTags } from "@nestjs/swagger";
 
 @Controller('posts')
+@ApiTags('Posts')
 export class PostController {
 
     constructor(private readonly postService:PostService, private readonly userService:UsersService){}
